c9e2daa8f9c0f8a8b1c514705c876d809dd5290f
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-11-19 17:29:52 -0300

    feat: transaction type badge

--- app/transactions/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-const Transaction = ({ params: { id } }) => {
-  return <h1>Transaction: {id}</h1>;
-};
-
-export default Transaction;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,23 @@
-const TransactionsPage = () => {
-  return <h1>Transactions Pages</h1>;
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between p-6">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
