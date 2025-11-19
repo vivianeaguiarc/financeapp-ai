3425931627cc6e5620de485f2747731c66289486
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-11-19 14:17:18 -0300

    feat: update fonts to mulish

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,5 @@
 const SubscriptionPage = () => {
-    return ( <h1>Subscription Page</h1> );
-}
- 
+  return <h1>Subscription Page</h1>;
+};
+
 export default SubscriptionPage;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,5 @@
 const TransactionsPage = () => {
-    return ( <h1>Transactions Pages</h1> );
-}
- 
+  return <h1>Transactions Pages</h1>;
+};
+
 export default TransactionsPage;
