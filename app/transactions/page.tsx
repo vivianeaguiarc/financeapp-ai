793a735264cfbaaef8d6473e6782198e4b55c538
Viviane Aguiar <vivianeaguiarc@outlook.com>
2025-11-21 16:14:27 -0300

    feat: ensure transactions tables labels are rendered properly

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -10,7 +10,7 @@ const TransactionsPage = async () => {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between p-6">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full">
+        <Button className="rounded-full font-bold">
           Adicionar Transação
           <ArrowDownUpIcon />
         </Button>
